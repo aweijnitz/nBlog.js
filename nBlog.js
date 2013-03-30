977a881cf9c97f133b9a27c2a175351f6a1e4344
Anders Weijnitz <anders.weijnitz@gmail.com>
2013-03-30 21:55:21 +0100

    Adding web resources

--- nBlog.js
+++ nBlog.js
@@ -1,7 +1,7 @@
 var compiler = require('./lib/compile.js');
 
 var SOURCE_PATH = "./posts";
-var DESTINATION_PATH = "./public";
+var DESTINATION_PATH = "./public/posts";
 
 // compiler.testCompile();
 compiler.compileDir(SOURCE_PATH, DESTINATION_PATH);
