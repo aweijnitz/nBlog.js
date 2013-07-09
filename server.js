64bbda68029638644bc91062150d10e6f8ca3436
Anders Weijnitz <anders.weijnitz@gmail.com>
2013-07-09 20:23:42 +0200

    Introduced config

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+var config = require('./config/config.json');
 var connect = require('connect');
 
-var dir = __dirname;
-var port = 80;
+if(!config) {
+    console.error('Could not find ./config/config.json. Make sure it is in the config folder (relative to nBlog.js).');
+    process.exit(1);
+}
+
+var dir = config.outputDir || __dirname;
+var port = config.port || 8080;
 
 var green  = '\033[32m';
 var reset = '\033[0m';
